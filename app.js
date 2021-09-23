52afc0546209e4351d10ad4cf12c35612cd96102
fe01134 <carlosyells@yahoo.com>
2021-09-23 08:36:54 +0100

    working serving static content MKDocs on IBM Cloud site

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 const express = require('express')
 const app = express();
 
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
 const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
+  res.send('Site is Under Maintenance  The Maximo Hands On Lab Site was using an NodeJS buildpack version that has become outdated. A new buildpack  is being tested for compatibility with MKDocs.  You can use the static lab content using the link below until the site is back up.  https://github.com/IBM/monitor-hands-on-lab/tree/master/MkDocs  We will be updating this notice with updates on when the site will be back up. Last updated 14:0 GMT Sept 22.  Sorry for the inconvenience.  ');
 })
 
 app.listen(port, host);
